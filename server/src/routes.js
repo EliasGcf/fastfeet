84580f9eaf94d6a6c5303b16ddcefadda08e9cb2
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-02-12 16:46:40 -0300

    Add novas rotas

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
 	return res.json({ ok: true });
 });
+
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware); // todas as rotas declaradas abaixo, deverão conter o token.
+
+routes.post('/recipients', RecipientController.store);
 
 export default routes;
