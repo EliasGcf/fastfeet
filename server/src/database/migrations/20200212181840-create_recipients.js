66fbecd1ed50d869b27dced1b457f9ad9e048ff9
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-02-12 16:45:53 -0300

    Migrations de User e Recipients

---
+++ server/src/database/migrations/20200212181840-create_recipients.js
@@ -0,0 +1,54 @@
+const Sequelize = require('sequelize');
+
+module.exports = {
+	up: queryInterface => {
+		return queryInterface.createTable('recipients', {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			street: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			number: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			complement: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+			},
+			state: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			city: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			zip_code: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			created_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			updated_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+		});
+	},
+
+	down: queryInterface => {
+		return queryInterface.dropTable('recipients');
+	},
+};
