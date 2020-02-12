b0ff035d32e3ea147e260d0f1e07eedf3a969360
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-02-12 16:46:19 -0300

    Add novos models

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -3,8 +3,10 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
 // Importar models e colocar no array
+import User from '../app/models/User';
+import Recipient from '../app/models/Recipient';
 
-const models = [];
+const models = [User, Recipient];
 
 class Database {
 	constructor() {
