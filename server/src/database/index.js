080580d9239ce081615d2de5e338d43df634215d
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-02-13 16:29:32 -0300

    Add novos models e Configuração de associate

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -5,8 +5,10 @@ import databaseConfig from '../config/database';
 // Importar models e colocar no array
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
+import File from '../app/models/File';
+import Deliveryman from '../app/models/Deliveryman';
 
-const models = [User, Recipient];
+const models = [User, Recipient, File, Deliveryman];
 
 class Database {
 	constructor() {
@@ -16,7 +18,9 @@ class Database {
 	init() {
 		this.connection = new Sequelize(databaseConfig);
 
-		models.map(model => model.init(this.connection));
+		models
+			.map(model => model.init(this.connection))
+			.map(model => model.associate && model.associate(this.connection.models));
 	}
 }
 
