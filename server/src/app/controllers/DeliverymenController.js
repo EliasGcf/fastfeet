5811535066f310cb0bf86f7c06afef45d370f828
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-02-13 16:28:58 -0300

    Criando Migration, Model, Controller de DeliveryM

---
+++ server/src/app/controllers/DeliverymenController.js
@@ -0,0 +1,109 @@
+import * as Yup from 'yup';
+
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
+
+class DeliverymenController {
+	async store(req, res) {
+		const schema = Yup.object().shape({
+			name: Yup.string().required(),
+			email: Yup.string()
+				.email()
+				.required(),
+		});
+
+		if (!(await schema.isValid(req.body))) {
+			return res.status(400).json({ error: 'validation fails' });
+		}
+
+		const { name, email } = req.body;
+
+		const deliverymanExists = await Deliveryman.findOne({ where: { email } });
+
+		/*
+		 * Check if delivery man already exists in database
+		 */
+		if (deliverymanExists) {
+			return res.status(400).json({ error: 'Delivery man already exists' });
+		}
+
+		const { id } = await Deliveryman.create({ name, email });
+
+		return res.json({ id, name, email });
+	}
+
+	async index(req, res) {
+		const deliverymen = await Deliveryman.findAll();
+
+		return res.json(deliverymen);
+	}
+
+	async update(req, res) {
+		const schema = Yup.object().shape({
+			name: Yup.string().required(),
+			email: Yup.string()
+				.email()
+				.required(),
+			avatar_id: Yup.number().notRequired(),
+		});
+
+		if (!(await schema.isValid(req.body))) {
+			return res.status(400).json({ error: 'validation fails' });
+		}
+
+		const { name, email, avatar_id } = req.body;
+
+		/*
+		 * Check if avatar_id exists in database
+		 */
+		if (avatar_id) {
+			const avatarExists = await File.findByPk(avatar_id);
+
+			if (!avatarExists) {
+				return res.status(400).json({ error: 'File does not exists' });
+			}
+		}
+
+		/*
+		 * Check if delivery man exists in database
+		 */
+		const { id } = req.params;
+
+		const deliveryman = await Deliveryman.findByPk(id);
+
+		if (!deliveryman) {
+			return res.status(400).json({ error: 'Delivery man does not exists' });
+		}
+
+		/*
+		 * Check if exists an other delivery man with the same email
+		 */
+		if (email !== deliveryman.email) {
+			const deliverymanExists = await Deliveryman.findOne({ where: { email } });
+
+			if (deliverymanExists) {
+				return res.status(400).json({ error: 'User already exists.' });
+			}
+		}
+
+		await deliveryman.update({ name, email, avatar_id });
+
+		return res.json({ id, name, email, avatar_id });
+	}
+
+	async destroy(req, res) {
+		const { id } = req.params;
+
+		return res.json({
+			id,
+			alert:
+				'Verificar em todas as tabelas que tem deliveryman_id, e só permita deletar, caso não haja nenhum registro desse id.',
+		});
+		/*
+			Verificar em todas as tabelas que tem deliveryman_id, e só permita
+			deletar, caso não haja nenhum registro desse id.
+		 */
+	}
+}
+
+export default new DeliverymenController();
