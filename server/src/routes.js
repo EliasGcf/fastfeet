dc048842f55e17865a647798578b5ffa03cd0f66
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-02-13 16:29:41 -0300

    Add novas rotas

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymenController from './app/controllers/DeliverymenController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -14,7 +16,14 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware); // todas as rotas declaradas abaixo, deverão conter o token.
 
+// Rotas de destinatarios
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+// Rotas de entregadores
+routes.post('/deliverymen', DeliverymenController.store);
+routes.get('/deliverymen', DeliverymenController.index);
+routes.put('/deliverymen/:id', DeliverymenController.update);
+routes.delete('/deliverymen/:id', DeliverymenController.destroy);
 
 export default routes;
