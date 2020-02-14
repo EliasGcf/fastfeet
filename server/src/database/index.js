b322c88a3b24792b2725a1295fc64d8bb7d5fbb1
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-02-14 16:54:09 -0300

    Add model no arquivo index.js

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -7,8 +7,9 @@ import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
 import File from '../app/models/File';
 import Deliveryman from '../app/models/Deliveryman';
+import Delivery from '../app/models/Delivery';
 
-const models = [User, Recipient, File, Deliveryman];
+const models = [User, Recipient, File, Deliveryman, Delivery];
 
 class Database {
 	constructor() {
