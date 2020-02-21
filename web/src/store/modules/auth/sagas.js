3b91b3ac612c2d09e886114722a7c887facb8149
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-02-21 19:11:56 -0300

    Add toastfy no redux-saga

--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -1,5 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
-// import { toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import history from '~/services/history';
@@ -23,7 +23,7 @@ export function* singIn({ payload }) {
 
 		history.push('/deliveries');
 	} catch (err) {
-		// toast.error('Falha na autenticação, verifique seus dados');
+		toast.error('Falha na autenticação, verifique seus dados');
 		yield put(signFailure());
 	}
 }
