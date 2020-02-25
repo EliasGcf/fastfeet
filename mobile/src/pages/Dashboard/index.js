6323a2d446458a3cfa508b188e698aaac02e1ffc
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-02-25 15:09:41 -0300

    Configurando vector icons e fonts

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 // import { Container } from './styles';
 
 export default function Dashboard() {
-	return <View />;
+	return (
+		<SafeAreaView
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+			}}
+		>
+			<Text
+				style={{
+					fontFamily: 'Roboto-Regular',
+					fontSize: 30,
+				}}
+			>
+				Elias
+			</Text>
+		</SafeAreaView>
+	);
 }
